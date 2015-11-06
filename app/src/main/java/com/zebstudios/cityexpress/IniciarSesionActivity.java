058cf3f65eb83e9aa46806b0f0db3cfc145e6592
Daniel Carreto <daniel.castillo@denumeris.com>
2015-11-05 19:28:15 -0600

    Se crea la vista de iniciar sesión y se corrigen errores al momento de compilar

---
+++ app/src/main/java/com/zebstudios/cityexpress/IniciarSesionActivity.java
@@ -0,0 +1,13 @@
+package com.zebstudios.cityexpress;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IniciarSesionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
