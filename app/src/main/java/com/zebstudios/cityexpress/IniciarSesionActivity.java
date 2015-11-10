a3f1ed5af1a3b762090c4aa0d2737f4df5cab367
Daniel Carreto <daniel.castillo@denumeris.com>
2015-11-10 17:07:09 -0600

    Se incluyen el toolbar para regresar y mostrar los títulos en registro e iniciar sesión.

--- app/src/main/java/com/zebstudios/cityexpress/IniciarSesionActivity.java
+++ app/src/main/java/com/zebstudios/cityexpress/IniciarSesionActivity.java
@@ -2,12 +2,27 @@ package com.zebstudios.cityexpress;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class IniciarSesionActivity extends Activity {
 
+    TextView tituloToolbar;
+    ImageView imageViewBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+        tituloToolbar = (TextView) findViewById(R.id.toolbarTitle);
+        imageViewBack = (ImageView) findViewById(R.id.back_button);
+
+        tituloToolbar.setText(getResources().getText(R.string.iniciar_sesion_toolbar));
+        imageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/zebstudios/cityexpress/RegistroActivity.java
+++ app/src/main/java/com/zebstudios/cityexpress/RegistroActivity.java
@@ -2,12 +2,30 @@ package com.zebstudios.cityexpress;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegistroActivity extends Activity {
+
+    TextView tituloToolbar;
+    ImageView imageViewBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        tituloToolbar = (TextView) findViewById(R.id.toolbarTitle);
+        imageViewBack = (ImageView) findViewById(R.id.back_button);
+
+        imageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
     }
 }
