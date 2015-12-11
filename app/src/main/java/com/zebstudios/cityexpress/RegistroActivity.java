c2e35729997bceaf63b36b0aa1386cf914453e93
Edwin Hernández <hgededwindaniel@gmail.com>
2015-12-11 17:47:40 -0600

    ...

--- app/src/main/java/com/zebstudios/cityexpress/RegistroActivity.java
+++ app/src/main/java/com/zebstudios/cityexpress/RegistroActivity.java
@@ -218,7 +218,7 @@ public class RegistroActivity extends Activity {
         imageConditions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageConditions.setImageDrawable(getDrawable(R.drawable.checked));
+                imageConditions.setImageResource(R.drawable.checked);
                 banderaCondiciones = true;
             }
         });
