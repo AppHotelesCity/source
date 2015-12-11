7ba55ee062cca567b6bb54134fb43c3e0982aca1
Daniel Castillo Carreto <dany7carreto@gmail.com>
2015-12-10 23:19:18 -0600

    Se recibe servicio soap con los detalles de disponibilidad.

--- app/src/main/java/com/zebstudios/cityexpress/ResultadosDisponibilidad.java
+++ app/src/main/java/com/zebstudios/cityexpress/ResultadosDisponibilidad.java
@@ -1,15 +1,25 @@
 package com.zebstudios.cityexpress;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -23,9 +33,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -44,6 +64,7 @@ public class ResultadosDisponibilidad extends FragmentActivity {
     private MapView _mapView;
     LatLng hotelPosition;
     Marker marker;
+    String cadena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,6 +143,53 @@ public class ResultadosDisponibilidad extends FragmentActivity {
             }
         });
 
+
+        cadena = "<soapenv:Envelope" +
+                "    xmlns:soapenv=http://schemas.xmlsoap.org/soap/envelope/" +
+                "    xmlns:tem=http://tempuri.org/" +
+                "    xmlns:cit=http://schemas.datacontract.org/2004/07/CityHub>" +
+                "    <soapenv:Header/>" +
+                "    <soapenv:Body>" +
+                "        <tem:GetRoomsAvailablePromo>" +
+                "            <tem:promoRequestModelv3>" +
+                "                <cit:CodigoPromocion></cit:CodigoPromocion>" +
+                "                <cit:CodigoTarifa>"+1114+"</cit:CodigoTarifa>" +
+                "                <cit:FechaInicial>2015-12-16</cit:FechaInicial>" +
+                "                <cit:Hotel>CJPAU</cit:Hotel>" +
+                "                <cit:NumeroAdultos>"+1+"</cit:NumeroAdultos>" +
+                "                <cit:NumeroDeNoches>"+1+"</cit:NumeroDeNoches>" +
+                "                <cit:NumeroHabitaciones>"+1+"</cit:NumeroHabitaciones>" +
+                "                <cit:Segmento></cit:Segmento>" +
+                "                <cit:TipoHabitacion></cit:TipoHabitacion>" +
+                "            </tem:promoRequestModelv3>" +
+                "        </tem:GetRoomsAvailablePromo>" +
+                "    </soapenv:Body>" +
+                "</soapenv:Envelope>";
+
+        cadena = "<soapenv:Envelope\n" +
+                "    xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"\n" +
+                "    xmlns:tem=\"http://tempuri.org/\"\n" +
+                "    xmlns:cit=\"http://schemas.datacontract.org/2004/07/CityHub\">\n" +
+                "    <soapenv:Header/>\n" +
+                "    <soapenv:Body>\n" +
+                "        <tem:GetRoomsAvailablePromo>\n" +
+                "            <tem:promoRequestModelv3>\n" +
+                "                <cit:CodigoPromocion></cit:CodigoPromocion>\n" +
+                "                <cit:CodigoTarifa>1114</cit:CodigoTarifa>\n" +
+                "                <cit:FechaInicial>2015-12-16</cit:FechaInicial>\n" +
+                "                <cit:Hotel>CJPAU</cit:Hotel>\n" +
+                "                <cit:NumeroAdultos>1</cit:NumeroAdultos>\n" +
+                "                <cit:NumeroDeNoches>1</cit:NumeroDeNoches>\n" +
+                "                <cit:NumeroHabitaciones>1</cit:NumeroHabitaciones>\n" +
+                "                <cit:Segmento></cit:Segmento>\n" +
+                "                <cit:TipoHabitacion></cit:TipoHabitacion>\n" +
+                "            </tem:promoRequestModelv3>\n" +
+                "        </tem:GetRoomsAvailablePromo>\n" +
+                "    </soapenv:Body>\n" +
+                "</soapenv:Envelope>";
+
+        enviarRegistro();
+
     }
     public void buscarDisponibilidad(String busqueda){
         System.out.println("->"+busqueda);
@@ -177,11 +245,80 @@ public class ResultadosDisponibilidad extends FragmentActivity {
             }
 
 
-/*            for (int i = 0; i < hoteles.length(); i++) {
+
+            /*for (int i = 0; i < hoteles.length(); i++) {
                 listaHotel.add(new Hotel(hoteles.get(i).toString()));
             }*/
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            //Log.i(TAG, "onPreExecute");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+           // Log.i(TAG, "doInBackground");
+            limpiarDatosUsuario();
+            //calculate();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            //Log.i(TAG, "onPostExecute");
+            //Toast.makeText(ResultadosDisponibilidad.this, "Response" , Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void limpiarDatosUsuario(){
+
+    }
+
+    public void enviarRegistro(){
+        StringRequest registro = new StringRequest(Request.Method.POST,"http://wshc.hotelescity.com:9742/wsMotor2014/ReservationEngine.svc", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println("->"+response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                NetworkResponse response = error.networkResponse;
+                String datos = new String(response.data);
+                System.out.println("sout" + datos);
+            }
+        }){
+
+            public String getBodyContentType(){
+                return "text/xml; charset=utf-8";
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError
+            {
+                HashMap<String, String>  params = new HashMap<String,String>();
+                //params.put("Content-Type", "application/xml; charset=utf-8");
+                params.put("SOAPAction", "http://tempuri.org/IReservationEngine/GetRoomsAvailablePromo");
+                Log.d("hsdhsdfhuidiuhsd","clave");
+                return params;
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return cadena.toString().getBytes();
+            }
+
+        };
+        System.out.println("registro->"+registro.toString());
+        registro.setRetryPolicy(new DefaultRetryPolicy(12000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        Volley.newRequestQueue(this).add(registro);
     }
 }
