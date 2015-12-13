1a1c830a2e773b1a80042e349b4564a7f4567a86
Daniel Castillo Carreto <dany7carreto@gmail.com>
2015-12-13 15:34:50 -0600

    Se agrega el Clima y Dirección

--- app/src/main/java/com/zebstudios/cityexpress/DetalleHotelActivity.java
+++ app/src/main/java/com/zebstudios/cityexpress/DetalleHotelActivity.java
@@ -1,13 +1,18 @@
 package com.zebstudios.cityexpress;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -21,6 +26,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONObject;
+
+import static com.appsee.Appsee.addEvent;
+
 public class DetalleHotelActivity extends ActionBarActivity {
 
     private ServicesListAdapter _serviciosListAdapter;
@@ -32,6 +41,7 @@ public class DetalleHotelActivity extends ActionBarActivity {
     private TextView txtServicios;
     private TextView txtMapa;
     private ListView listView;
+    private WeatherReport _weatherReport;
     private LinearLayout linearLayoutDescripcion;
 
     @Override
@@ -47,6 +57,10 @@ public class DetalleHotelActivity extends ActionBarActivity {
         linearLayoutDescripcion = (LinearLayout) findViewById(R.id.linearDescripcion);
 
         //Detalles
+
+        TextView lblAddress = (TextView) findViewById(R.id.lblAddress);
+        String address = getAddressString();
+        lblAddress.setText( address );
 
         _imageCache = new ImageCache(this );
         String[] images = new String[ResultadosDisponibilidad.listaGeneralHotel.get(0).get_imagenes().length];
@@ -74,6 +88,43 @@ public class DetalleHotelActivity extends ActionBarActivity {
             lblDescription.setText(ResultadosDisponibilidad.listaGeneralHotel.get(0).getDescripcionMaps());
         }
 
+        ImageButton btnCall = (ImageButton) findViewById( R.id.btnCall );
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = "tel:" + ResultadosDisponibilidad.listaGeneralHotel.get(0).getTelefono();
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse(uri));
+                startActivityForResult(intent, 0);
+                //Appsee
+                addEvent("HotelDetails-Call-Smartphone");
+            }
+        });
+
+        ImageButton btnEmail = (ImageButton) findViewById( R.id.btnMail );
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String to = ResultadosDisponibilidad.listaGeneralHotel.get(0).getEmail();
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+                email.setType("message/rfc822");
+                startActivityForResult(Intent.createChooser(email, "Send Email"), 1);
+                addEvent("HotelDetails-SendEmail-Smartphone");
+            }
+        });
+
+        ImageButton btnChat = (ImageButton) findViewById( R.id.btnChat );
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("btnChat", "Web View Chat");
+                Uri chatwv = Uri.parse("http://chat.hotelescity.com/WebAPISamples76/Chat/HtmlChatFrameSet.jsp");
+                Intent intent = new Intent(Intent.ACTION_VIEW, chatwv);
+                startActivity(intent);
+                addEvent("HotelDetails-Chat-Smartphone");
+            }
+        });
 
         //Servicios
         _serviciosListAdapter = new ServicesListAdapter(this, ResultadosDisponibilidad.listaGeneralHotel.get(0).getServicios() );
@@ -136,7 +187,7 @@ public class DetalleHotelActivity extends ActionBarActivity {
             }
         });
 
-
+        new GetWeather().execute();
 
     }
 
@@ -144,6 +195,41 @@ public class DetalleHotelActivity extends ActionBarActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+
+    private String getAddressString()
+    {
+        String address = "";
+        if( ResultadosDisponibilidad.listaGeneralHotel.get(0).getDireccion() != null && !ResultadosDisponibilidad.listaGeneralHotel.get(0).getDireccion().equalsIgnoreCase( "null" ) )
+        {
+            address += ResultadosDisponibilidad.listaGeneralHotel.get(0).getDireccion() + "\n";
+        }
+        if( ResultadosDisponibilidad.listaGeneralHotel.get(0).getColonia() != null && !ResultadosDisponibilidad.listaGeneralHotel.get(0).getColonia().equalsIgnoreCase( "null" ) )
+        {
+            address += ResultadosDisponibilidad.listaGeneralHotel.get(0).getColonia() + "\n";
+        }
+        if( ResultadosDisponibilidad.listaGeneralHotel.get(0).getCp() != null && !ResultadosDisponibilidad.listaGeneralHotel.get(0).getCp().equalsIgnoreCase( "null" ) )
+        {
+            address += "CP: " + ResultadosDisponibilidad.listaGeneralHotel.get(0).getCp() + "\n";
+        }
+        if( ResultadosDisponibilidad.listaGeneralHotel.get(0).getCiudad() != null && !ResultadosDisponibilidad.listaGeneralHotel.get(0).getCiudad().equalsIgnoreCase( "null" ) && ResultadosDisponibilidad.listaGeneralHotel.get(0).getEstado() != null && !ResultadosDisponibilidad.listaGeneralHotel.get(0).getEstado().equalsIgnoreCase( "null" ) )
+        {
+            address += ResultadosDisponibilidad.listaGeneralHotel.get(0).getCiudad() + ". " + ResultadosDisponibilidad.listaGeneralHotel.get(0).getEstado();
+        }
+        else if( ResultadosDisponibilidad.listaGeneralHotel.get(0).getCiudad() != null && !ResultadosDisponibilidad.listaGeneralHotel.get(0).getCiudad().equalsIgnoreCase( "null" ) )
+        {
+            address += ResultadosDisponibilidad.listaGeneralHotel.get(0).getCiudad();
+        }
+        else if( ResultadosDisponibilidad.listaGeneralHotel.get(0).getEstado() != null && !ResultadosDisponibilidad.listaGeneralHotel.get(0).getEstado().equalsIgnoreCase( "null" ) )
+        {
+            address += ResultadosDisponibilidad.listaGeneralHotel.get(0).getEstado();
+        }
+        if( address.endsWith( "\n" ) )
+        {
+            address = address.substring( 0, address.length() - 1 );
+        }
+        return address;
     }
 
     private class ImagePagerAdapter extends PagerAdapter
@@ -187,6 +273,94 @@ public class DetalleHotelActivity extends ActionBarActivity {
         public void destroyItem( ViewGroup container, int position, Object object )
         {
             container.removeView((ImageView) object);
+        }
+    }
+
+
+    public void weatherObtained()
+    {
+        LinearLayout panel = (LinearLayout) findViewById( R.id.tblWeather );
+        panel.setVisibility( View.VISIBLE );
+
+        if( _weatherReport.getWeathers().length > 0 )
+        {
+            ImageView imgWeather = (ImageView) findViewById( R.id.imgWeather );
+            imgWeather.setImageResource( GetResourceForWeatherIcon( _weatherReport.getWeathers()[0].getIcon() ) );
+
+            TextView lblDesc = (TextView) findViewById(R.id.lblWDesc);
+            lblDesc.setText( _weatherReport.getWeathers()[0].getDescription() );
+        }
+
+        TextView lblTemp = (TextView) findViewById(R.id.lblWTemp);
+        lblTemp.setText( String.format( "%.2f", _weatherReport.getTemperature() ) + "°C" );
+    }
+
+    private int GetResourceForWeatherIcon( String icon )
+    {
+        if( icon.equalsIgnoreCase( "01d" ) ) return R.drawable.w_01d;
+        else if( icon.equalsIgnoreCase( "01n" ) ) return R.drawable.w_01n;
+        else if( icon.equalsIgnoreCase( "02d" ) ) return R.drawable.w_02d;
+        else if( icon.equalsIgnoreCase( "02n" ) ) return R.drawable.w_02n;
+        else if( icon.equalsIgnoreCase( "03d" ) ) return R.drawable.w_03d;
+        else if( icon.equalsIgnoreCase( "03n" ) ) return R.drawable.w_03n;
+        else if( icon.equalsIgnoreCase( "04d" ) ) return R.drawable.w_04d;
+        else if( icon.equalsIgnoreCase( "04n" ) ) return R.drawable.w_04n;
+        else if( icon.equalsIgnoreCase( "09d" ) ) return R.drawable.w_09d;
+        else if( icon.equalsIgnoreCase( "09n" ) ) return R.drawable.w_09n;
+        else if( icon.equalsIgnoreCase( "10d" ) ) return R.drawable.w_10d;
+        else if( icon.equalsIgnoreCase( "10n" ) ) return R.drawable.w_10n;
+        else if( icon.equalsIgnoreCase( "11d" ) ) return R.drawable.w_11d;
+        else if( icon.equalsIgnoreCase( "11n" ) ) return R.drawable.w_11n;
+        else if( icon.equalsIgnoreCase( "13d" ) ) return R.drawable.w_13d;
+        else if( icon.equalsIgnoreCase( "13n" ) ) return R.drawable.w_13n;
+        else if( icon.equalsIgnoreCase( "50d" ) ) return R.drawable.w_50d;
+        else if( icon.equalsIgnoreCase( "50n" ) ) return R.drawable.w_50n;
+        else return R.drawable.w_01d;
+    }
+
+
+    private class GetWeather extends AsyncTask<Void, Void, Integer>
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground( Void... arg0 )
+        {
+            ServiceHandler sh = new ServiceHandler();
+            String jsonStr = sh.makeServiceCall( "http://api.openweathermap.org/data/2.5/weather?lat=" + ResultadosDisponibilidad.listaGeneralHotel.get(1).getLatitude() + "&lon=" + ResultadosDisponibilidad.listaGeneralHotel.get(1).getLongitude() + "&lang=ES&units=metric", ServiceHandler.GET );
+
+            if( jsonStr != null )
+            {
+                try
+                {
+                    JSONObject json = new JSONObject( jsonStr );
+                    _weatherReport = new WeatherReport( json );
+                    if( !_weatherReport.isParsedOk() )
+                        return -3;
+
+                    return 0;
+                }
+                catch( Exception e )
+                {
+                    android.util.Log.e( "JSONParser", "Cant parse: " + e.getMessage() );
+                    return -2;
+                }
+            }
+            else android.util.Log.e( "ServiceHandler", "Couldn't get any data" );
+
+            return -1;
+        }
+
+        @Override
+        protected void onPostExecute( Integer result )
+        {
+            super.onPostExecute(result);
+
+            if( result == 0 ) weatherObtained();
         }
     }
 }
