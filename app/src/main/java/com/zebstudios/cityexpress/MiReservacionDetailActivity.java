561b801cd664038bf2c9c762a71f87c367a36785
Edwin Hernández <hgededwindaniel@gmail.com>
2015-12-21 12:50:59 -0600

    agregando iconos. Haciendo Portrait la app

--- app/src/main/java/com/zebstudios/cityexpress/MiReservacionDetailActivity.java
+++ app/src/main/java/com/zebstudios/cityexpress/MiReservacionDetailActivity.java
@@ -1,7 +1,17 @@
 package com.zebstudios.cityexpress;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by edwinhernandez on 21/12/15.
  */
-public class MiReservacionDetailActivity {
+public class MiReservacionDetailActivity extends Activity {
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mi_reservacion_detail_activity);
+    }
 }
